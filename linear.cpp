d656d50dea8f12d6da47eef42aa02df8e0af7ee6
Jagruti Sanjay Patil <102649317+jagrutipatil3030@users.noreply.github.com>
2023-05-04 22:12:39 +0530

    Create linear.cpp

---
+++ linear.cpp
@@ -0,0 +1,52 @@
+#include<iostream.h>
+#include<conio.h>
+class linear1
+{
+  public:
+     int arr[100],i.size,ele;
+     void get();
+     void show();
+};
+void linear1 :: get()
+{
+   cout<<"Enter the size of Array";
+   cin>>size;
+  
+  cout<<"Enter Array Element :\n";
+  for(i=0;i<size;i++)
+  {
+    cin>>arr[i];
+  }
+  cout<<"Array Element are :\n";
+  for(i=0;i<size;i++)
+  {
+    cout<<arr[i]<<" ";
+  }
+}   
+void linear1 :: show ()
+{
+  cout<<"Enter Element to Search";
+    cin>>ele;
+  for(i=0;I,size;i++)
+  {
+    if(arr[i]==ele)
+    {
+      cout<<"Element is found At"<<i;
+      break;
+    }
+  }
+  if(i==size)
+  {
+    cout<<"Element is not found";
+  } 
+}
+void main()
+{
+  clrscr();
+      linear1 l1;
+        l1.get();
+        l1.show();
+    getch();
+}
+  
+       
